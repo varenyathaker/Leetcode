20c0b7dfe3c123938f0d6649f3191ee53f9e6a7e
varenyathaker <varenyathaker375@gmail.com>
2024-09-26 08:02:19 +0530

    53 updated

--- 53_maximum_subarray.cpp
+++ 53_maximum_subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int sum = 0 ; 
-        int max = 0 ; 
+        int max = INT_MIN ; 
         for(int i = 0; i < nums.size() ;i++){
             sum += nums[i]; 
             if(sum > max){
